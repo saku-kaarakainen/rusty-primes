612833b7d94f7d57dc87411902f50af63ad7fbf8
Saku Kaarakainen <sakuba91@hotmail.com>
2023-02-07 13:53:56 +0200

    added tutorial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use yew::prelude::*;
+
+struct Model {
+    value: i64
+}
+
+// This is like a React component
+#[function_component(App)]
+fn app() -> Html {
+    // This is like useState in React
+    let state = use_state(|| Model { 
+        value: 0 
+    });
+
+    // no semicolon after the last because, 
+    // otherwise onclick would return nothing, because it's a closure.
+    let onclick = {
+        // Shadows the original object
+        let state = state.clone();
+        
+        Callback::from(move |_| {
+            state.set(Model {
+                value: state.value + 1
+           })
+        })
+    };
+
+    // This is like render in React    
+    html! {
+        <div>
+            // {onclick} is shorthand for {onclick: onclick}
+            <button {onclick}>{ "+1" }</button>
+            <p>{ state.value }</p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Reference to the app component
+    yew::start_app::<App>();
 }
